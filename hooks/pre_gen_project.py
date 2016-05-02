0e9056b0fb35fa08fed6c8ffc6351b6d45d0c197
Alexander Reelsen <alexander@reelsen.net>
2016-05-02 16:10:32 +0200

    Removed package_path variable, allowed processor name to consist only of lowercase letters

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,11 +4,11 @@ import re
 import sys
 import os
 
-REGEX = r'^[_a-zA-Z]+$'
+REGEX = r'^[_a-z]+$'
 processor_type = '{{ cookiecutter.processor_type }}'
 
 if not re.match(REGEX, processor_type):
-    print('ERROR: %s is not a valid processor type name! Use only alphabetic chars.' % processor_type)
+    print('ERROR: %s is not a valid processor type name! Use only alphabetic lower case chars.' % processor_type)
     # exits with status 1 to indicate failure
     sys.exit(1)
 
